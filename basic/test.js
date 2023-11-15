cb6a198dec0f4b737ae4a7aae789806e1e5f1f54
Ibtisam Haider <140896804+Ibtisamhaider12@users.noreply.github.com>
2023-11-15 09:05:38 +0000

    basic

--- basic/test.js
+++ basic/test.js
@@ -1,1 +1,32 @@
-console.log("ibtisam")
+// let score = "ibtisam"
+
+// console.log(typeof score);
+// console.log(typeof (score));
+
+// let valueInNumber = Number (score)
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+
+// let isLoggedIn = "ibt"
+
+// let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn);
+
+// let Number = 33
+
+// let stringNumber = String(Number)
+// console.log(typeof stringNumber);
+
+// Operations //
+
+// let value = 3
+// let negValue = -value
+// console.log(negValue);
+
+let str1 = "hello"
+let str2 = " ibtisam"
+let str3 = " haider"
+
+let str4 = str1 + str2 + str3
+console.log(str4);
